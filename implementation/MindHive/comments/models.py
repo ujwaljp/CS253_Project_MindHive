2e5bba2c7802848cbc1e48e826330ada29bc2231
Ujwal Jyot Panda <ujwal12366@gmail.com>
2022-03-06 16:09:48 +0530

    Comment model

--- implementation/MindHive/comments/models.py
+++ implementation/MindHive/comments/models.py
@@ -1,8 +1,11 @@
 from tkinter import CASCADE
 from django.db import models
 from ..MindHive.models import Content
+from ..questions.models import Question
+from ..answers.models import Answer
 
 # Create your models here.
 class Comment(Content):
-    parentObjType : models.CharField(max_length=200)
-    parentObj : models.ForeignKey(Content, on_delete=CASCADE)
+    parentObjType = models.CharField(max_length=200)
+    parentObjQ = models.ForeignKey(Question, on_delete=CASCADE)
+    parentObjA = models.ForeignKey(Answer, on_delete=CASCADE)
