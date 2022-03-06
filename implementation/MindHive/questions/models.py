07a906fd8dddc15682969fae890cbdb13db8b637
harsh500 <harsh.trivedi.mikki@gmail.com>
2022-03-06 16:37:39 +0530

    added app for tags

--- implementation/MindHive/questions/models.py
+++ implementation/MindHive/questions/models.py
@@ -2,9 +2,10 @@ from pyexpat import model
 from django.db import models
 from ..MindHive.models import Content
 from ..users.models import User
+from ..tags.models import Tag
 class Question(Content):
     # Question-specific fields
     title = models.CharField(max_length=200)
-    tags = models.ManyToManyField(Tag)
+    tags = models.ManyToManyField(Tag,related_name='relatedto')
     numViews = models.IntegerField(default=0)
     viewedBy = models.ManyToManyField(User, related_name='viewedBy')
