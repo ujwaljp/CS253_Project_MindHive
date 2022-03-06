ed06302487ee1a11f09fb0859b9f0cd30779a463
ris27hav <rishav20@iitk.ac.in>
2022-03-06 15:05:40 +0530

    add user model

--- implementation/MindHive/users/models.py
+++ implementation/MindHive/users/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=20)
+    name = models.CharField(max_length=20)
+    email = models.EmailField()
+    password = models.CharField(max_length=20)
+    profile_image = models.ImageField(upload_to='profile_image', blank=True)
+    blocked = models.BooleanField(default=False)
+    followingQuestions = models.ManyToManyField('Question', blank=True)
+    bookmarkQuestions = models.ManyToManyField('Question', blank=True)
+    favouriteTags = models.ManyToManyField('Tag', blank=True)
+    notifications = models.ManyToManyField('Notification', blank=True)
+
+    def __str__(self):
+        return self.username
