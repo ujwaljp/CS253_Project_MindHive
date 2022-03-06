a892a90679ef3286a5fbaa38b5ef3abcd1118957
harsh500 <harsh.trivedi.mikki@gmail.com>
2022-03-06 16:26:42 +0530

    added question model and remove quotes form onetoone.....

--- implementation/MindHive/answers/models.py
+++ implementation/MindHive/answers/models.py
@@ -1,3 +1,6 @@
 from django.db import models
-
+from ..MindHive.models import Content
+from ..questions.models import Question
 # Create your models here.
+class Answer(Content):
+    parent_obj=models.ForeignKey(Question, on_delete=models.CASCADE)
--- implementation/MindHive/questions/models.py
+++ implementation/MindHive/questions/models.py
@@ -1,9 +1,10 @@
+from pyexpat import model
 from django.db import models
 from ..MindHive.models import Content
-
+from ..users.models import User
 class Question(Content):
     # Question-specific fields
     title = models.CharField(max_length=200)
-    tags = models.ManyToManyField('Tag')
+    tags = models.ManyToManyField(Tag)
     numViews = models.IntegerField(default=0)
-    viewedBy = models.ManyToManyField('User', related_name='viewedBy')
+    viewedBy = models.ManyToManyField(User, related_name='viewedBy')
