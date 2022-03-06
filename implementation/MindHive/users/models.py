400ba0ee8b0b42872ce89dd4ac50f49ba133219c
ris27hav <rishav20@iitk.ac.in>
2022-03-06 16:58:07 +0530

    add report

--- implementation/MindHive/users/models.py
+++ implementation/MindHive/users/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
-# Create your models here.
+from ..answers.models import Answer
+from ..questions.models import Question
+from ..comments.models import Comment
+
 class User(models.Model):
     username = models.CharField(max_length=20)
     name = models.CharField(max_length=20)
@@ -15,3 +18,16 @@ class User(models.Model):
 
     def __str__(self):
         return self.username
+
+class Report(models.Model):
+    report_text = models.CharField(max_length=200)
+    reporter = models.ForeignKey(User, on_delete=models.CASCADE)
+    reportedUser = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reported')
+    reportedObjType = models.CharField(max_length=20)
+    reportedObjQ = models.ForeignKey(Question, on_delete=models.CASCADE, null=True, blank=True)
+    reportedObjA = models.ForeignKey(Answer, on_delete=models.CASCADE, null=True, blank=True)
+    reportedObjC = models.ForeignKey(Comment, on_delete=models.CASCADE, null=True, blank=True)
+    report_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.report_text
