08e28f3537a2420b1171de46697c870bdab09a89
Ujwal Jyot Panda <ujwal12366@gmail.com>
2022-03-09 17:43:47 +0530

    Small fix

--- implementation/MindHive/MindHive/urls.py
+++ implementation/MindHive/MindHive/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('users.urls'))
+    path('', include('users.urls')),
     path('admin/', admin.site.urls),
 ]
