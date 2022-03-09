ecad96dc8d90257d9af10d87905d542742f9f248
harsh500 <harsh.trivedi.mikki@gmail.com>
2022-03-09 23:21:51 +0530

    Added the function in views of all the apps

--- implementation/MindHive/users/urls.py
+++ implementation/MindHive/users/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     # eg: /users/1/
     path('<int:user_id>', views.view, name='view_user'),
      # eg: /users/1/edit/
-    path('<int:user_id>/edit', views.ask, name='edit_users'),
+    path('<int:user_id>/edit', views.edit, name='edit_users'),
 ]
--- implementation/MindHive/users/views.py
+++ implementation/MindHive/users/views.py
@@ -3,14 +3,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import User
 # Create your views here.
-def profile(request):
+def view(request):
     usr1 = User()
     usr1.username = 'ujp'
     usr1.name = 'Ujwal'
     usr1.email = 'ujwaljp20'
     usr1.password = 'ggopwp'
     usr1.blocked = False
-
     usr = [usr1]
     # return render(request, 'profile.html', {'users': usr})
     return HttpResponse("Hello, world. You're at the polls index.")
+def edit(request):
+    # return render(request, 'profile.html', {'users': usr})
+    return HttpResponse("Here the edit page will come")
