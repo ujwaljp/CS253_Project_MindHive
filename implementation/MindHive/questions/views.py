0a2faedfa1523e1cebe35a510e9141b4a8e9b73d
Ujwal Jyot Panda <ujwal12366@gmail.com>
2022-03-10 17:02:32 +0530

    Check add question function

--- implementation/MindHive/questions/views.py
+++ implementation/MindHive/questions/views.py
@@ -1,4 +1,11 @@
 from django.http import HttpResponse
+import sys
+sys.path.append("..")
+from .models import Question
+from users.models import User
+from django.shortcuts import render, get_object_or_404
+import datetime
+
 
 # Create your views here.
 def view(request, question_id):
@@ -7,5 +14,11 @@ def view(request, question_id):
 def edit(request, question_id):
     return HttpResponse("You're editing question %s." % question_id)
 
-def ask(request):
-    return HttpResponse("You're asking a question.")
+def ask(request, user_id):
+    return render(request, 'questions/ask.html')
+
+def submit(request, user_id):
+    # user = User.objects.filter(id = user_id).values('User')
+    user = get_object_or_404(User, id = user_id)
+    newQuestion = Question.objects.create(text = request.POST.get('text',False), pub_date = datetime.datetime.now(), author = user)
+    return render(request, 'questions/submit.html', {'Ques' : newQuestion})
