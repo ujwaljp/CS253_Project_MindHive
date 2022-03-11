cb2832ddeb65844457d8dcd69e08cf54f9061005
ris27hav <rishav20@iitk.ac.in>
2022-03-12 01:53:30 +0530

    fix content model

--- implementation/MindHive/home/models.py
+++ implementation/MindHive/home/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 class Content(models.Model):
     text = models.CharField(max_length=200)
-    author = models.OneToOneField(to='users.User', on_delete=models.CASCADE)
+    author = models.ForeignKey(to='users.User', on_delete=models.CASCADE, related_name='author')
     pub_date = models.DateTimeField('date published')
     numLikes = models.IntegerField(default=0)
-    numDislikes = models.IntegerField(default=0)
     likedBy = models.ManyToManyField(to='users.User', related_name='likedBy', blank=True)
+    numDislikes = models.IntegerField(default=0)
     dislikedBy = models.ManyToManyField(to='users.User', related_name='dislikedBy', blank=True)
 
     def __str__(self):
