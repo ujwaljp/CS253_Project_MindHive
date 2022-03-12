2a1e71ada4f0009c8dd7bb7a47c188b1cf55ead2
Ujwal Jyot Panda <ujwal12366@gmail.com>
2022-03-12 16:02:56 +0530

    Attempt to add tags

--- implementation/MindHive/questions/views.py
+++ implementation/MindHive/questions/views.py
@@ -6,6 +6,7 @@ from .models import Question
 from users.models import User
 from django.shortcuts import render, get_object_or_404
 import datetime
+from tags.models import Tag
 def view(request, question_id):
     question = Question.objects.get(id=question_id)
     return render(request, 'questions/view_ques.html', {'question': question})
@@ -14,10 +15,11 @@ def edit(request, question_id):
     return HttpResponse("You're editing question %s." % question_id)
 
 def ask(request, user_id):
-    return render(request, 'questions/ask.html')
+    tags = Tag.objects.all()
+    return render(request, 'questions/ask.html', {'tags' : tags})
 
 def submit(request, user_id):
     # user = User.objects.filter(id = user_id).values('User')
     user = get_object_or_404(User, id = user_id)
-    newQuestion = Question.objects.create(text = request.POST.get('question',False), pub_date = datetime.datetime.now(), author = user,title=request.POST.get('title',False))
+    newQuestion = Question.objects.create(text = request.POST.get('question',False), pub_date = datetime.datetime.now(), author = user,title=request.POST.get('title',False),tags=request.POST.get('tags',False))
     return render(request, 'questions/view_ques.html', {'question' : newQuestion})
