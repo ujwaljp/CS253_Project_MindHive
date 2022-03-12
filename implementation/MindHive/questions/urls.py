429bfee7ee465269117bcff5d4b5fc544da19f9c
harsh500 <harsh.trivedi.mikki@gmail.com>
2022-03-12 15:19:05 +0530

    added html files for asking questions and submit and changed the url of questions

--- implementation/MindHive/questions/urls.py
+++ implementation/MindHive/questions/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     # eg: /questions/edit/1/
     path('<int:question_id>/edit/', views.edit, name='edit_question'),
     # eg: /questions/ask/
-    path('ask/', views.ask, name='ask_question'),
+    path('<int:user_id>/ask/', views.ask, name='ask'),
+    path('<int:user_id>/ask/submit', views.submit, name='submit')
 ]
