2aac34f2f1ca28c6c1545420525aaf6585dc0509
harsh500 <harsh.trivedi.mikki@gmail.com>
2022-03-13 10:01:48 +0530

    added form for question

---
+++ implementation/MindHive/questions/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from .models import Question
+import sys
+sys.path.append("..")
+from tags.models import Tag
+import datetime
+class CreateQuestionForm(forms.ModelForm):    
+    class Meta:
+        model = Question
+        fields = ["title","text","tags"] 
+        title = forms.CharField()    
+        tags = forms.ModelMultipleChoiceField(
+        queryset=Tag.objects.all(),
+        widget=forms.CheckboxSelectMultiple
+        )
