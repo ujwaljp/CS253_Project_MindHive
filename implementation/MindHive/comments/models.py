e03ba19b051afa68c3a8e6b2ffd0cc03d0c4e192
ris27hav <rishav20@iitk.ac.in>
2022-03-13 15:23:29 +0530

    fix some models

--- implementation/MindHive/comments/models.py
+++ implementation/MindHive/comments/models.py
@@ -3,6 +3,10 @@ from home.models import Content
 
 # Create your models here.
 class Comment(Content):
-    parentObjType = models.CharField(max_length=200)
+    PARENT_OBJECT_TYPE = (
+        ('q', 'Question'),
+        ('a', 'Answer'),
+    )
+    parentObjType = models.CharField(max_length=1, choices=PARENT_OBJECT_TYPE)
     parentObjQ = models.ForeignKey(to='questions.Question', on_delete=models.CASCADE, blank=True, null=True)
     parentObjA = models.ForeignKey(to='answers.Answer', on_delete=models.CASCADE, blank=True, null=True)
