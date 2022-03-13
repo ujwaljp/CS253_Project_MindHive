c0c929a15b035f2fe4d53c95f69ac00754c599f4
ris27hav <rishav20@iitk.ac.in>
2022-03-13 14:53:00 +0530

    fix issues

--- implementation/MindHive/questions/views.py
+++ implementation/MindHive/questions/views.py
@@ -1,14 +1,12 @@
-from sre_constants import SUCCESS
-from turtle import title
-from urllib import request
 from django.http import HttpResponse
 from .forms import CreateQuestionForm
 import sys
-sys.path.append("..")
 from .models import Question
-from users.models import User
 from django.shortcuts import render
 from django.views.generic.edit import CreateView
+
+sys.path.append("..")
+
 def view(request, question_id):
     question = Question.objects.get(id=question_id)
     return render(request, 'questions/view_ques.html', {'question': question})
