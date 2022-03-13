788aa27c8f3be3a12d11ac97fc73c0d5e4f02a8c
pmaniar20 <pmaniar20@iitk.ac.in>
2022-03-14 01:40:12 +0530

    added search

--- implementation/MindHive/home/urls.py
+++ implementation/MindHive/home/urls.py
@@ -1,8 +1,8 @@
-
 from django.urls import path
 from . import views
 
 urlpatterns = [
     # eg: /home/1/
     path('<int:user_id>', views.view, name='view_home'),
+    path('searchbar/', views.searchbar, name='searchbar'),
 ]
--- implementation/MindHive/home/views.py
+++ implementation/MindHive/home/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from questions.models import Question
 # Create your views here.
 def view(request):
     return HttpResponse("Home page")
+
+def searchbar(request):
+    if request.method == 'GET':
+        search = request.GET.get('search')
+        post = Question.object.all().filter(title=search)
+        return render(request, 'searchbar.html', {'post':post})
