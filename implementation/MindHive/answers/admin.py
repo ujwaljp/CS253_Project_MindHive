14411cede937024312dcdbf92f940430086697f9
Rishav Bikarwar <rishav20@iitk.ac.in>
2022-03-14 18:26:52 +0530

    modify admin interface

--- implementation/MindHive/answers/admin.py
+++ implementation/MindHive/answers/admin.py
@@ -3,7 +3,7 @@ from .models import Answer
 # Register your models here.
 @admin.register(Answer)
 class AnswerAdmin(admin.ModelAdmin):
-    list_display = ('text', 'author', 'pub_date', 'ques')
+    list_display = ('text', 'author', 'pub_date', 'parentObj')
     fieldsets = (
         (None, {
             'fields': ('text', 'ques', ('author', 'pub_date'), 'anonymous')
