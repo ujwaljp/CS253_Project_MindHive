2f8cf131519e00b6a4be3204bb00dd9fad4ffe4a
Ujwal Jyot Panda <ujwal12366@gmail.com>
2022-03-14 22:07:19 +0530

    Home page info for questions add

--- implementation/MindHive/home/views.py
+++ implementation/MindHive/home/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from questions.models import Question
+from users.models import User
 # Create your views here.
-def view(request):
-    return HttpResponse("Home page")
+def view(request, user_id):
+    user = User.objects.filter(id = user_id).values_list('favouriteTags')
+    interestQues = Question.objects.filter(tags__in = user)
+    # return HttpResponse(interestQues)
+    return render(request, 'home/home.html', {'questions' : interestQues})
 
 def searchbar(request):
     if request.method == 'GET':
