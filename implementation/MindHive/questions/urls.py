eb05fb829f7980dbf17e1d3d53e5d11dfa404765
Rishav Bikarwar <rishav20@iitk.ac.in>
2022-03-14 15:15:56 +0530

    add upvote/downvote in question

--- implementation/MindHive/questions/urls.py
+++ implementation/MindHive/questions/urls.py
@@ -5,7 +5,11 @@ from . import views
 urlpatterns = [
     # eg: /questions/1/
     path('<int:question_id>', views.view, name='view_question'),
-    # eg: /questions/edit/1/
+    # eg: /questions/1/like/
+    path('<int:question_id>/like/', views.like, name='like_question'),
+    # eg: /questions/1/dislike/
+    path('<int:question_id>/dislike/', views.dislike, name='dislike_question'),
+    # eg: /questions/1/edit/
     path('<int:question_id>/edit/', views.edit, name='edit_question'),
     # eg: /questions/ask/
     path('<int:user_id>/askform', views.QuestionCreateView.as_view()),
