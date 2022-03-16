f0d1ce9f2a71d1b5653ebcdfa791099822f0e857
pmaniar20 <pmaniar20@iitk.ac.in>
2022-03-16 19:40:40 +0530

    updated search

--- implementation/MindHive/home/urls.py
+++ implementation/MindHive/home/urls.py
@@ -6,11 +6,7 @@ app_name='home'
 urlpatterns = [
     # eg: /home/1/
     path('', views.view, name='view_home'),
-<<<<<<< HEAD
     path('search_results', views.search_results, name='search_results'),
-=======
     path('bookmarks/', views.bookView, name='bookmarks'),
     path('following/', views.folView, name='following'),
-    path('searchbar/', views.searchbar, name='searchbar'),
->>>>>>> 3ae204d9051ff8ab2e785a179968653e5fb58b24
 ]
