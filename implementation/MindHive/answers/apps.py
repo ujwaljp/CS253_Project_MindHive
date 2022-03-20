9cc83637cb0a9066a53061b4816ce1b682319311
Rishav Bikarwar <rishav20@iitk.ac.in>
2022-03-20 21:15:05 +0530

    ad comments for answers code

--- implementation/MindHive/answers/apps.py
+++ implementation/MindHive/answers/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
 
 
+# AnswersConfig class to register the answers app with Django.
 class AnswersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'answers'
