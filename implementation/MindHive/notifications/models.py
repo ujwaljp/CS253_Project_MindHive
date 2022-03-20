3e33efaf39ed83d2335c913a77eb9c392f4fdfb1
Rishav Bikarwar <rishav20@iitk.ac.in>
2022-03-20 21:21:44 +0530

    add comments for notificationss app

--- implementation/MindHive/notifications/models.py
+++ implementation/MindHive/notifications/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+# Notification model
 class Notification(models.Model):
     receivers = models.ManyToManyField(to='users.User', related_name='notifications', blank=True)
     text = models.CharField(max_length=200)
     timestamp = models.DateTimeField(auto_now_add=True)
     target_question = models.ForeignKey(to='questions.Question', on_delete=models.CASCADE, null=True)
 
+    #  __str__ method to return the text of the notification on calling str(notification)
     def __str__(self):
         return self.text
