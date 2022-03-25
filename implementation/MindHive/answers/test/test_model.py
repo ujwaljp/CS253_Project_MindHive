6431ecc694b5d2a88233d06d148fe2df3f3bb46b
Adi Pratap Singh <apsinghnsa@gmail.com>
2022-03-26 02:51:30 +0530

    test forms

--- implementation/MindHive/answers/test/test_model.py
+++ implementation/MindHive/answers/test/test_model.py
@@ -1,5 +1,9 @@
 from django.test import TestCase
+from users.models import User
+from tags.models import Tag
 from answers.models import Answer
+from ckeditor.fields import RichTextField
+from datetime import datetime
 
 class AnswerModelTest(TestCase):
     @classmethod
---
+++ implementation/MindHive/questions/tests/test_forms.py
@@ -0,0 +1,51 @@
+from django import forms
+from django.test import TestCase
+from matplotlib import widgets
+from questions.models import Question
+from notifications.models import Notification
+from questions.forms import CreateQuestionForm
+from questions.forms import AddAnswerForm
+from questions.forms import CreateReportForm
+from users.models import User
+from tags.models import Tag
+from ckeditor.fields import RichTextField
+from datetime import datetime
+import pytz
+class Setup_Class(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(email="user@iitk.ac.in", username="user", password="cscbndm",name="user")
+        Tag.objects.create(name="Anarchy")
+        question = Question.objects.create(title='test',tags='Anarchy')
+
+class QuestionFormTest(TestCase):
+    def test_valid(self):
+        tag=Tag.objects.create(name="Anarchy")
+        form_data = {'title': 'user',
+                     'text': 'Usertext',
+                    #  'tags': "Anarchy",
+                     'anonymous':False}
+        form = CreateQuestionForm(data = form_data)
+        self.assertFalse(form.is_valid())
+        saved = form.save()
+        self.assertEqual(saved.title, "user")
+        self.assertEqual(saved.text, "Usertext")
+        # self.assertEqual(saved.tags, "Anarchy")
+        self.assertEqual(saved.author, "user")
+        self.assertEqual(saved.timestamp.date(),datetime.now().date())
+
+class AnswerFormTest(TestCase):
+    def test_valid(self):
+        form_data = {'title': 'user',
+                     'text': 'Usertext',
+                    #  'tags': "Anarchy",
+                     'anonymous':False}
+        form = CreateQuestionForm(data = form_data)
+        self.assertFalse(form.is_valid())
+        saved = form.save()
+        self.assertEqual(saved.title, "user")
+        self.assertEqual(saved.text, "Usertext")
+        # self.assertEqual(saved.tags, "Anarchy")
+        self.assertEqual(saved.author, "user")
+        self.assertEqual(saved.timestamp.date(),datetime.now().date())
+
+    
