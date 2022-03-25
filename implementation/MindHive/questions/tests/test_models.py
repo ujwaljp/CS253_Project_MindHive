738075bf4f9ff5de4eec1f02f714011a4d84afec
harsh500 <harsh.trivedi.mikki@gmail.com>
2022-03-25 18:58:25 +0530

    added questions test in model

--- implementation/MindHive/questions/tests/test_models.py
+++ implementation/MindHive/questions/tests/test_models.py
@@ -14,6 +14,7 @@ class QuestionModelTest(TestCase):
         tag=Tag.objects.create(name="tag1")
         User.objects.create(email="example@example.com", username="eg", password="example")
         question=Question.objects.create(text=text_content, author=user1, anonymous=False, pub_date=datetime(2020,1,1,0,0,0,tzinfo=pytz.UTC),title="How to do this?")
+        question=Question.objects.create(text=text_content, author=user2, anonymous=False, pub_date=datetime(2020,1,1,0,0,0,tzinfo=pytz.UTC),title="How to do this?")
         question.likedBy.add(user1)
         question.dislikedBy.add(user2)
         question.viewedBy.add(user1)
@@ -28,4 +29,43 @@ class QuestionModelTest(TestCase):
         question = Question.objects.get(id=1)
         expected_object_name = question.title
         self.assertEqual(str(question), expected_object_name)
+
+    def test_content_richfield(self):
+        question = Question.objects.get(id=1)
+        expected_object_name = question.text
+        self.assertEqual(str(RichTextField("So I have a question")), expected_object_name)
     
+    def test_tag_check(self):
+        question = Question.objects.get(id=1)
+        expected_object_name = question.tags.get(id=1).name
+        self.assertEqual("tag1", expected_object_name)
+
+    def test_likedBy_check(self):
+        question = Question.objects.get(id=1)
+        expected_object_name = question.likedBy.get(id=1).username
+        self.assertEqual("eg", expected_object_name)
+
+    def test_dislikedBy_check(self):
+        question = Question.objects.get(id=1)
+        expected_object_name = question.dislikedBy.get(id=2).username
+        self.assertEqual("eg2", expected_object_name)
+
+    def test_viewedby_check(self):
+        question = Question.objects.get(id=1)
+        expected_object_name = question.viewedBy.get(id=1).username
+        self.assertEqual("eg", expected_object_name)
+    
+    def test_viewedby_check(self):
+        question = Question.objects.get(id=1)
+        expected_object_name = question.viewedBy.get(id=1).username
+        self.assertEqual("eg", expected_object_name)
+    
+    def test_check_pubdate(self):
+        question = Question.objects.get(id=1)
+        expected_object_name = question.pub_date
+        self.assertEqual(expected_object_name, datetime(2020,1,1,0,0,0,tzinfo=pytz.UTC))
+
+    def test_check_author(self):
+        question = Question.objects.get(id=1)
+        expected_object_name = question.author.username
+        self.assertEqual("eg", expected_object_name)
