f8b0f29a40a6ee539328a9f7a3f17b0e02d7b150
harsh500 <harsh.trivedi.mikki@gmail.com>
2022-03-25 22:05:44 +0530

    added the html styling to change passowrd pages and also added the test for notification models

--- implementation/MindHive/questions/tests/test_models.py
+++ implementation/MindHive/questions/tests/test_models.py
@@ -9,16 +9,20 @@ class QuestionModelTest(TestCase):
     @classmethod
     def setUpTestData(cls):
         text_content=RichTextField("So I have a question")
-        user1 = User.objects.create(email="example@iitk.ac.in", username="eg", password="example")
-        user2 = User.objects.create(email="example2@iitk.ac.in", username="eg2", password="example2")
+        user1 = User.objects.create(email="example@iitk.ac.in", username="eg", password="example",name="harish")
+        user2 = User.objects.create(email="example2@iitk.ac.in", username="eg2", password="example2",name="harish2")
+        user3 = User.objects.create(email="example3@iitk.ac.in", username="eg3", password="example3",name="harish3")
         tag=Tag.objects.create(name="tag1")
         User.objects.create(email="example@example.com", username="eg", password="example")
-        question=Question.objects.create(text=text_content, author=user1, anonymous=False, pub_date=datetime(2020,1,1,0,0,0,tzinfo=pytz.UTC),title="How to do this?")
-        question.likedBy.add(user1)
-        question.dislikedBy.add(user2)
-        question.viewedBy.add(user1)
-        question.tags.add(tag)
-        question.save()
+        question1=Question.objects.create(text=text_content, author=user1, anonymous=False, pub_date=datetime(2020,1,1,0,0,0,tzinfo=pytz.UTC),title="How to do this?")
+        question2=Question.objects.create(text=text_content, author=user2, anonymous=True, pub_date=datetime(2020,1,1,1,1,1,tzinfo=pytz.UTC),title="I am anonymous")
+        question1.likedBy.add(user1)
+        question1.likedBy.add(user3)
+        question1.dislikedBy.add(user2)
+        question1.viewedBy.add(user1)
+        question1.tags.add(tag)
+        question1.save()
+        question2.save()
     def test_title_max_length(self):
         question = Question.objects.get(id=1)
         max_length = question._meta.get_field('title').max_length
@@ -54,7 +58,6 @@ class QuestionModelTest(TestCase):
         expected_object_name = question.viewedBy.get(id=1).username
         self.assertEqual("eg", expected_object_name)
     
-    
     def test_check_pubdate(self):
         question = Question.objects.get(id=1)
         expected_object_name = question.pub_date
@@ -64,3 +67,28 @@ class QuestionModelTest(TestCase):
         question = Question.objects.get(id=1)
         expected_object_name = question.author.username
         self.assertEqual("eg", expected_object_name)
+
+    def test_check_anonymous(self):
+        question = Question.objects.get(id=2)
+        expected_object_name = question.anonymous
+        self.assertEqual(expected_object_name, True)
+
+    def test_get_author_name(self):
+        question1 = Question.objects.get(id=1)
+        expected_object_name = question1.get_author_name()
+        self.assertEqual(expected_object_name, 'harish')
+        question2 = Question.objects.get(id=2)
+        expected_object_name = question2.get_author_name()
+        self.assertEqual(expected_object_name, 'Anonymous User')
+
+    def test_get_absolute_url(self):
+        question = Question.objects.get(id=2)
+        expected_object_name = question.get_absolute_url()
+        self.assertEqual(expected_object_name, '/questions/2')
+    
+    def test_get_votes(self):
+        question = Question.objects.get(id=1)
+        expected_object_name = question.get_votes()
+        self.assertEqual(expected_object_name, 1)
+
+    
