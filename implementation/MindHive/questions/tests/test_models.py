370684c32d6cf379cfc99af6ddddd9ed9025b73c
harsh500 <harsh.trivedi.mikki@gmail.com>
2022-03-25 21:00:05 +0530

    remove the error in model test for question

--- implementation/MindHive/questions/tests/test_models.py
+++ implementation/MindHive/questions/tests/test_models.py
@@ -14,7 +14,6 @@ class QuestionModelTest(TestCase):
         tag=Tag.objects.create(name="tag1")
         User.objects.create(email="example@example.com", username="eg", password="example")
         question=Question.objects.create(text=text_content, author=user1, anonymous=False, pub_date=datetime(2020,1,1,0,0,0,tzinfo=pytz.UTC),title="How to do this?")
-        question=Question.objects.create(text=text_content, author=user2, anonymous=False, pub_date=datetime(2020,1,1,0,0,0,tzinfo=pytz.UTC),title="How to do this?")
         question.likedBy.add(user1)
         question.dislikedBy.add(user2)
         question.viewedBy.add(user1)
@@ -55,10 +54,6 @@ class QuestionModelTest(TestCase):
         expected_object_name = question.viewedBy.get(id=1).username
         self.assertEqual("eg", expected_object_name)
     
-    def test_viewedby_check(self):
-        question = Question.objects.get(id=1)
-        expected_object_name = question.viewedBy.get(id=1).username
-        self.assertEqual("eg", expected_object_name)
     
     def test_check_pubdate(self):
         question = Question.objects.get(id=1)
