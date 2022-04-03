20b2a514fa8212b2823c31fa697a473a6b7ddfbc
Ujwal Jyot Panda <ujwal12366@gmail.com>
2022-04-03 17:28:45 +0530

    Question forms testing done except report

--- implementation/MindHive/questions/tests/test_forms.py
+++ implementation/MindHive/questions/tests/test_forms.py
@@ -8,15 +8,18 @@ from questions.forms import AddAnswerForm
 from questions.forms import CreateReportForm
 from users.models import User
 from tags.models import Tag
+from answers.models import Answer
+from comments.models import Comment
 from ckeditor.fields import RichTextField
 from datetime import datetime
 import pytz
-class Setup_Class(TestCase):
-    def setUp(self):
-        self.user = User.objects.create(email="user@iitk.ac.in", username="user", password="cscbndm",name="user")
-        user1 = User(email="example@iitk.ac.in", username="eg", password="example",name="harish")
-        user1.save()
-        tag = Tag.objects.create(name="Anarchy")
+# class Setup_Class(TestCase):
+    # def setUp(self):
+        # self.user = User.objects.create(email="user@iitk.ac.in", username="user", password="cscbndm",name="user")
+        # user1 = User(email="example@iitk.ac.in", username="eg", password="example",name="harish")
+        # user1.save()
+        # tag = Tag.objects.create(name="Anarchy")
+
         # question = Question.objects.create(title='test',tags='Anarchy')
 
 class QuestionFormTest(TestCase):
@@ -56,4 +59,23 @@ class AnswerFormTest(TestCase):
         # self.assertEqual(saved.author, user)
         # self.assertEqual(saved.timestamp.date(),datetime.now().date())
 
-    
+class ReportFormTest(TestCase):
+    def test_valid(self):
+        user = User.objects.create(email="user@iitk.ac.in", username="user", password="cscbndm",name="user")
+        user1 = User.objects.create(email="example@iitk.ac.in", username="eg", password="example",name="harish")
+        tag = Tag.objects.create(name="Anarchy")
+        question = Question.objects.create(title='test',author=user)
+        question.tags.set([tag])
+        # answer = Answer.objects.create(text='Usertext',author=user,to_question=question)
+        # comment = Comment.objects.create(text='Usertext',author=user)
+        form_data = {'report_text': 'Usertext',
+                     'reporter': user,
+                     'reported_user': user1,
+                     'reportedObjType': 'q',
+                     'reportedObjQ': question,
+                     'reportedObjA': None,
+                     'reportedObjC': None}
+        form = CreateReportForm(data = form_data)
+        self.assertTrue(form.is_valid())
+        saved = form.save()
+        self.assertEqual(saved.report_text, "Usertext")
