10b6e507d9f5ec5a6e7877630ea70f9c06291594
harsh500 <harsh.trivedi.mikki@gmail.com>
2022-04-03 21:00:08 +0530

    removed useless question from db

---
+++ implementation/MindHive/seleniumtests/test_reportquestionchecks.py
@@ -0,0 +1,42 @@
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+class TestReportquestionchecks():
+  def setup_method(self, method):
+    self.driver = webdriver.Firefox()
+    self.vars = {}
+  
+  def teardown_method(self, method):
+    self.driver.quit()
+  
+  def test_reportquestionchecks(self):
+    self.driver.get("http://127.0.0.1:8000/")
+    self.driver.maximize_window()
+    self.driver.find_element(By.ID, "signin").click()
+    self.driver.find_element(By.ID, "id_password").send_keys("wsxc1234")
+    self.driver.find_element(By.ID, "id_username").send_keys("tharsh20@iitk.ac.in")
+    self.driver.find_element(By.CSS_SELECTOR, ".btn").click()
+    self.driver.find_element(By.ID, "askButton").click()
+    self.driver.find_element(By.ID, "id_title").click()
+    self.driver.find_element(By.ID, "id_title").send_keys("test question")
+    self.driver.switch_to.frame(0)
+    self.driver.find_element(By.CSS_SELECTOR, "p").click()
+    element = self.driver.find_element(By.CSS_SELECTOR, ".cke_editable")
+    self.driver.execute_script("if(arguments[0].contentEditable === 'true') {arguments[0].innerText = '<p>question for report<br></p>'}", element)
+    self.driver.switch_to.default_content()
+    self.driver.find_element(By.ID, "id_tags_1").click()
+    self.driver.find_element(By.CSS_SELECTOR, "input:nth-child(10)").click()
+    self.driver.find_element(By.CSS_SELECTOR, ".fa-ban").click()
+    self.driver.find_element(By.ID, "id_report_text").click()
+    self.driver.find_element(By.ID, "id_report_text").send_keys("offensive")
+    self.driver.find_element(By.CSS_SELECTOR, "form > input:nth-child(4)").click()
+    self.driver.find_element(By.CSS_SELECTOR, ".nav-item:nth-child(3) #dropdownMenuButton").click()
+    self.driver.find_element(By.LINK_TEXT, "Logout").click()
+  
+
+testClass = TestReportquestionchecks()
+
+testClass.setup_method("")
+testClass.test_reportquestionchecks()
+testClass.teardown_method("")
