3d756b63ab1664b4d9deae1cb8480ed4341743a1
harsh500 <harsh.trivedi.mikki@gmail.com>
2022-04-03 21:01:52 +0530

    error fix

--- implementation/MindHive/seleniumtests/test_follownotificationtest.py
+++ implementation/MindHive/seleniumtests/test_follownotificationtest.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 
+from selenium.webdriver.common.keys import Keys
 
 class TestFollownotificationtest():
   def setup_method(self, method):
