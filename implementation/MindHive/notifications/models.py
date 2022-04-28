d64b16009215b0eb5ef1a72e389cc2de5c949b99
lgokulnath <lgokulnath02@gmail.com>
2022-04-28 23:00:57 +0530

    add sort for notifs

--- implementation/MindHive/notifications/models.py
+++ implementation/MindHive/notifications/models.py
@@ -9,6 +9,9 @@ class Notification(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
     target_question = models.ForeignKey(
         to='questions.Question', on_delete=models.CASCADE, blank=True, null=True)
+    
+    class Meta:
+        ordering = ['-timestamp',]
 
     def __str__(self):
         """return the text of the notification on str(notification) call"""
